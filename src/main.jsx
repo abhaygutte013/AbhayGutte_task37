4715a8e9c9971f8a8fc50cb584c97bc6ee5d0d72
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-22 19:14:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "/src/App.jsx";
-import "/src/index.css";
-
-import { WorkoutsContextProvider } from "./context/WorkoutContext.jsx";
+import App from "./App";
+import { WorkoutContextProvider } from "./context/WorkoutContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
-  </React.StrictMode>
+  <WorkoutContextProvider>
+    <App />
+  </WorkoutContextProvider>
 );
