151b3c453115bbb33dfb39d9c1f7c177100fe29d
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-22 12:00:24 +0530

    Update import path for WorkoutsContext

--- src/hooks/useWorkoutContext.js
+++ src/hooks/useWorkoutContext.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { WorkoutsContext } from "../context/WorkoutContext";
+import { WorkoutsContext } from "/src/context/WorkoutContext.jsx";
 
 const useWorkoutContext = () => {
   const context = useContext(WorkoutsContext);
