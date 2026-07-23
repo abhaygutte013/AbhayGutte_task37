b85919514383f0f29f4695d43ea804d3a0ff0b1d
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-23 10:43:23 +0530

    Refactor server.js to use ES6 imports and mongoose

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-const connectDB = require("./config/db");
-const workoutRoutes = require("./routes/workouts");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import workoutRoutes from "./routes/workouts.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
-
-app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
 
 app.use("/api/workouts", workoutRoutes);
 
-const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    app.listen(process.env.PORT, () => {
+      console.log("Server running on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
