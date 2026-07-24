cf21bc3092044e4c5360e5cf6a8321399cf74786
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-24 09:52:57 +0530

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import workoutRoutes from "./routes/workouts.js";
 
 dotenv.config();
 
 const app = express();
+
+// middleware
+app.use(cors());
 
 app.use(express.json());
 
@@ -14,8 +19,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// routes
 app.use("/api/workouts", workoutRoutes);
 
+// connect to database
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
