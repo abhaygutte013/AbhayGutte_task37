221854f283db530419cbf1f89bf7ef64a1e67fd3
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-24 09:59:32 +0530

    Update Workoutdetails.jsx

--- src/components/Workoutdetails.jsx
+++ src/components/Workoutdetails.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const API = import.meta.env.VITE_API_URL;
+fetch(API);
 
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
